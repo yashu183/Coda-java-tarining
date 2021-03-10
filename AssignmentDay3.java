6b17484b1a60fb1e1a9d9ff4c683110805178689
yashu183 <70622860+yashu183@users.noreply.github.com>
2021-03-10 14:10:42 +0530

    Add files via upload

---
+++ AssignmentDay3.java
@@ -0,0 +1,134 @@
+
+package assignDay3;
+
+//import com.sun.tools.javac.launcher.Main;
+
+abstract class MineBase { 
+    abstract void amethod(); 
+    static int i; 
+}
+
+public class AssignmentDay3 /*extends MineBase*/ {
+	//question - 1
+		//	float f=1.3; 
+		//	char c="a"; 
+		//	byte b=257; 
+		//	boolean b=null;
+/* all are wrong */		
+	static int i;
+	public static void main(String[] args) {
+        int anar[]=new int[]{1,2,3};
+        System.out.println(anar[1]);
+        int anar1[]=new int[5];
+        System.out.println(anar1[0]);
+        System.out.println(i);
+        int[] ar=new int[5]; 
+        for(i=0;i < ar.length;i++) {
+                System.out.println(ar[i]); 
+        } 
+//        int i=1; 
+//        switch (i) { 
+//                case 0: 
+//                System.out.println("zero"); 
+//        break; 
+//        case 1: 
+//                System.out.println("one"); 
+//        case 2: 
+//                System.out.println("two"); 
+//        default: 
+//                System.out.println("default"); 
+//        }
+//	}
+	
+	//int i=9; 
+//	switch (i) { 
+//	        default: 
+//	        System.out.println("default"); 
+//	                case 0: 
+//	        System.out.println("zero"); 
+//	                break; 
+//	        case 1: 
+//	                System.out.println("one"); 
+//	        case 2: 
+//	        System.out.println("two"); 
+//	}
+//	
+//}
+	
+//	int i = 0;
+//	if (i)
+//	{
+//		System.out.println("Hello");
+//	}
+	
+        boolean b=true; 
+        boolean b2=true; 
+        if(b==b2) { 
+                System.out.println("So true"); 
+                }
+        
+//        int i=1; 
+//        int j=2; 
+//        if(i==1 &| j==2) 
+//                System.out.println("OK");
+}}
+
+
+
+//question - 2
+//class MyClass { 
+//	static int i;
+//    public static void main(String arguments[]) { 
+//            //amethod(arguments); 
+//    	System.out.println(i);
+//    	
+//    	/* cannot make a static referrence to amethod */
+//    	
+//            } 
+//    public void amethod(String[] arguments) { 
+//            System.out.println(arguments); 
+//            System.out.println(arguments[1]); 
+//    } 
+//}
+
+//question - 3
+//package assignDay3; 
+//import java.awt.*; 
+//class Myclass {}
+
+//question - 4
+/* bye size -127 to 128 */
+
+//question - 5
+/* option 4 */ /* index out of bound */
+
+
+//question -6 
+/* except then all are reserved */
+
+//question - 7
+
+
+
+//question - 8
+/* output 0 bcz static var are initialized to zero*/
+
+//question - 9 
+/* ans - 3 */
+
+//questiuon - 10
+/* ans 0 */
+
+//question 11
+/* series of zeroes will be printed */
+
+//question - 12
+/* one two default */
+
+//question - 13
+/* deafult zero */
+
+//question - 14
+/* opt 2,3 */
+
+//question - 15
