392b951c12a9e4dded8ece99ec93f586ef7fba6a
Chinthakommadinne Yashwanth Kumar <70622860+yashu183@users.noreply.github.com>
2021-03-25 11:06:19 +0530

    Add files via upload

---
+++ Day9_Lab_Assignment/Day9_Lab_Assignment_Vehicle/FourWheelers.java
@@ -0,0 +1,11 @@
+package day9_lab_vehicle;
+
+public class FourWheelers {
+	public FourWheelers()
+	{
+		System.out.println("Four Wheelers cls....");
+	}
+	public static void main(String[] args) {
+		System.out.println("Four Wheelers cls.....");
+	}
+}
